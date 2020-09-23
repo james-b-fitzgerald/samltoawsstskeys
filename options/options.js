a29dc957cf9156fcdfaa25aee13c2ab5c8cc7253
james-b-fitzgerald <james.bryant.fitzgerald@gmail.com>
2020-09-22 17:22:34 -0700

    Update to allow for requesting different session durations and saving to different files for specific roles. (#1)
    Co-authored-by: james-fitzgerald <james.fitzgerald@salesforce.com>

--- options/options.js
+++ options/options.js
@@ -1,7 +1,7 @@
 // Saves options to chrome.storage
 function save_options() {
-  // Get the filename to be saved
-  var FileName = document.getElementById('FileName').value;
+  // Get the default filename to be saved
+  var DefaultFileName = document.getElementById('DefaultFileName').value;
   
   // Does SessionDuration needs to be applied?
 	var ApplySessionDuration = $("#SessionDuration option:selected").val();
@@ -9,25 +9,26 @@ function save_options() {
 	// Is DEBUG log enabled?
   var DebugLogs = $("#DebugLogs option:selected").val();
 
-  // Get the Role_ARN's (Profile/ARNs pairs) entered by the user in the table
-  var RoleArns = {};
-  // Iterate over all added profiles in the list
-  $("input[id^='profile_']").each(function( index ) {  
-	  // Replace profile_<rowId> for arn_<rowId> to be able to get value of corresponding arn input field
-	  var input_id_arn = $(this).attr('id').replace("profile", "arn");
-	  // Create key-value pair to add to RoleArns dictionary.
-	  // Only add it to the dict if both profile and arn are not an empty string
-	  if ($(this).val() != '' && $('#' + input_id_arn).val() != '') {
-		  RoleArns[$(this).val()] = $('#' + input_id_arn).val();
+  // Get the role arn/file/sessionDuration tuples entered by the user in the table
+  var Roles = {};
+  // Iterate over all added roles in the list
+  $("input[id^='arn_']").each(function( index ) {  
+	  // Replace arne_<rowId> for file_<rowId> and session_duration_<rowId> to be able to get value of corresponding file and session duration input fields
+	  var input_id_file = $(this).attr('id').replace("arn", "file");
+	  var input_id_session_duration = $(this).attr('id').replace("arn", "session_duration");
+	  // Create key-value pair to add to roles dictionary. Value is nested map with file and session duration
+	  // Only add it to the dict if all three values are non-empty
+	  if ($(this).val() != '' && $('#' + input_id_file).val() != '' && $('#' + input_id_session_duration).val() != '') {
+		Roles[$(this).val()] = {"File": $('#' + input_id_file).val(), "SessionDuration": $('#' + input_id_session_duration).val()};
 	  }
   });
   
   // Do the actual saving into Chrome storage
   chrome.storage.sync.set({
-    FileName: FileName,
+    DefaultFileName: DefaultFileName,
 		ApplySessionDuration: ApplySessionDuration,
 		DebugLogs: DebugLogs,
-	  RoleArns: RoleArns
+	  Roles: Roles
   }, function() {
     // Show 'Options saved' message to let user know options were saved.
     var status = document.getElementById('status');
@@ -47,40 +48,40 @@ function save_options() {
 function restore_options() {
   chrome.storage.sync.get({
 	// Default values
-    FileName: 'credentials',
+    DefaultFileName: 'credentials',
 		ApplySessionDuration: 'yes',
 		DebugLogs: 'no',
-	  RoleArns: {}
+	  Roles: {}
   }, function(items) {
 	// Set filename
-    document.getElementById('FileName').value = items.FileName;
+    document.getElementById('DefaultFileName').value = items.DefaultFileName;
     // Set ApplySessionDuration
 		$("#SessionDuration").val(items.ApplySessionDuration);
 		// Set DebugLogs
     $("#DebugLogs").val(items.DebugLogs);
-	// Set the html for the Role ARN's Table
-	$("#role_arns").html('<table><tr id="tr_header"><th>Profile</th><th>ARN of the role</th><th></th><th></th></tr></table>');
-	// For each profile/ARN pair add table row (showing the profile-name and ARN)
-	for (var profile in items.RoleArns){
-		if (items.RoleArns.hasOwnProperty(profile)) {
-			addTableRow('#tr_header', profile, items.RoleArns[profile]);
+	// Set the html for the Roles Table
+	$("#roles").html('<table><tr id="tr_header"><th>Arn</th><th>file</th><th>Session Duration</th><th></th><th></th></tr></table>');
+	// For each role add table row (showing the role arn, file, and session duration)
+	for (var arn in items.Roles){
+		if (items.Roles.hasOwnProperty(arn)) {
+			addTableRow('#tr_header', arn, items.Roles[arn]["File"], items.Roles[arn]["SessionDuration"]);
 		}
 	}
-	// Add a blank table row if there are now current entries (So the user can easily add a new profile/ARN pair)
-	if (Object.keys(items.RoleArns).length == 0) {
-		addTableRow('#role_arns table tr:last', null, null);
+	// Add a blank table row if there are now current entries (So the user can easily add a new role)
+	if (Object.keys(items.Roles).length == 0) {
+		addTableRow('#roles table tr:last', null, null, null);
 	}
   });
 }
 
-// Add a blank table row for the user to add a new profile/ARN pair
-function addTableRow(previousRowJquerySelector, profile, arn) {
+// Add a blank table row for the user to add a new role
+function addTableRow(previousRowJquerySelector, arn, file, sessionDuration) {
 	// Generate random identifier for the to be added row
 	var newRowId = randomId();
-	$(previousRowJquerySelector).after(getTableRowHtml(newRowId, profile, arn));
+	$(previousRowJquerySelector).after(getTableRowHtml(newRowId, arn, file, sessionDuration));
 	// Add eventHandlers for the newly added buttons
 	$('#btn_add_' + newRowId).on("click", function() {
-		addTableRow('#tr_' + newRowId, null, null);
+		addTableRow('#tr_' + newRowId, null, null, null);
 	});
 	$('#btn_del_' + newRowId).on("click", function() {
 		delTableRow('#tr_' + newRowId);
@@ -93,17 +94,20 @@ function delTableRow(tableRowJquerySelector) {
 	$(tableRowJquerySelector).remove();
 }
 
-// Generate HTML for a table row of the role_arns table
-function getTableRowHtml(tableRowId, profile, arn) {
-	var profileValue = '';
+// Generate HTML for a table row of the roles table
+function getTableRowHtml(tableRowId, arn, file, sessionDuration) {
 	var arnValue = '';
-	// If profile and arn are not NULL, generate HTML value attribute
-	if (profile) {profileValue = 'value="' + profile + '"'};
+	var fileValue = '';
+	var sessionDurationValue = '';
+	// If name, file, and session duration are not NULL, generate HTML value attribute
 	if (arn) {arnValue = 'value="' + arn + '"'};
+	if (file) {fileValue = 'value="' + file + '"'};
+	if (sessionDuration) {sessionDurationValue = 'value="' + sessionDuration + '"'};
 	// Generate HTML for the row
 	var html =	'<tr id="tr_' + tableRowId + '">\
-				<th><input type="text" id="profile_' + tableRowId + '" size="18" ' + profileValue + '></th> \
-				<th><input type="text" id="arn_' + tableRowId + '" size="55" ' + arnValue + '></th> \
+				<th><input type="text" id="arn_' + tableRowId + '" size="50" ' + arnValue + '></th> \
+				<th><input type="text" id="file_' + tableRowId + '" size="18" ' + fileValue + '></th> \
+				<th><input type="text" id="session_duration_' + tableRowId + '" size="18" ' + sessionDurationValue + '></th> \
 				<th><button id="btn_del_' + tableRowId + '">DEL</button></th> \
 				<th><button id="btn_add_' + tableRowId + '">ADD</button></th> \
 				</tr>';
